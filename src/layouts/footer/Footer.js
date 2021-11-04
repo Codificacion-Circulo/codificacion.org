c29f0b3ed7b36c0c50affc81dd9e69a13e8c15f4
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-04 21:47:11 +0530

    footer setup

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -2,74 +2,48 @@ import React from 'react'
 
 function Footer(props) {
     return (
-        <footer class="bg-light text-center text-white">
+        <footer className="bg-light text-center text-white">
 
-  <div class="container p-4 pb-0">
+            <div className="container p-4 pb-0">
 
-    <section class="mb-4">
-
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style={{backgroundColor: "#3b5998"}}
-        href="#!"
-        role="button"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+                <section className="mb-4">
 
 
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style={{backgroundColor: "#55acee"}}
-        href="#!"
-        role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
-
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style={{backgroundColor:"#dd4b39"}}
-        href="#!"
-        role="button"
-        ><i class="fab fa-google"></i
-      ></a>
+                    <a className="mx-2" style={{ color: "#dd2a7b" }} href="#!" role="button"
+                    ><i class="fab fa-instagram fa-lg"></i
+                    ></a>
 
 
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style={{backgroundColor:"#ac2bac"}}
-        href="#!"
-        role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
+                    <a className="mx-2" style={{ color: "#0077b5" }} href="#!" role="button"
+                    ><i class="fab fa-linkedin fa-lg"></i
+                    ></a>
 
-
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style={{backgroundColor:"#0082ca"}}
-        href="#!"
-        role="button"
-        ><i class="fab fa-linkedin-in"></i
-      ></a>
-
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style={{backgroundColor:"#333333"}}
-        href="#!"
-        role="button"
-        ><i class="fab fa-github"></i
-      ></a>
-    </section>
-
-  </div>
+                    <a className="mx-2" style={{ color: "#5865f2" }} href="#!" role="button"
+                    ><i class="fab fa-discord fa-lg"></i
+                    ></a>
 
 
 
-  <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-    © 2020 Copyright:
-    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
 
-</footer>
+                    <a className="mx-2" style={{ color: "#6e5494" }} href="#!" role="button"
+                    ><i class="fab fa-github fa-lg"></i
+                    ></a>
+
+
+
+
+                </section>
+
+            </div>
+
+
+
+            <div className="text-center p-3 text-dark" style={{ backgroundColor: "#e3f2fd" }}>
+                © 2021 Copyright:
+                <a style={{ textDecoration: "none" }} className="text-dark mx-2" href="https://codificacion.org/">Codificacion.org</a>
+            </div>
+
+        </footer>
     )
 }
 
